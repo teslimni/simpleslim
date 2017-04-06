2b1f1898b0a50c97855c85d841da385c6481bdb4
Teslim Adeyemo <teslimni@gmail.com>
2017-04-06 19:52:25 +0100

    Created dev branch and added slim-twig and some view reources

--- index.php
+++ index.php
@@ -9,14 +9,38 @@ $app = new \Slim\App([
 ]);
 
 $container = $app->getContainer();
+
 $container['view'] = function ($container) {
 	$view = new \Slim\Views\Twig(__DIR__ . '/resources/views', [
 		'cache' => false
 	]);
+
+	// Instantiate and add Slim specific extension
+	$basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+	$view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+	return $view;
 };
 
-$app->get('/', function() {
-	echo "Salam Home";
+$app->get('/', function($request, $response) {
+	return $this->view->render($response, 'home.twig');
+});
+
+$app->get('/users', function($request, $response) {
+
+	$user = [
+		'name' => 'Amina',
+		'email' => 'minatesssy@temina.com'
+
+	];
+
+	return $this->view->render($response, 'users.twig', [
+
+		'user' => $user,
+
+	]);
+
+
 });
 
 $app->run();
