27b63403dd1d9420a01bff7e205655629296b30d
Teslim Adeyemo <teslimni@gmail.com>
2017-04-10 10:45:28 +0100

    Added a welcome text to the home route.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php 
 use App\Controllers\TopicController;
 
+$app->get('/', function() {
+	echo 'Welcome to simpleslim';
+});
 $app->group('/topics', function () {
 	$this->get('', TopicController::class . ':index');
 	$this->get('/{id}', TopicController::class . ':show');
