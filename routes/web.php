d82df6fdcdb4f7dfcb20e92584fcdcad7361ffdf
Teslim Adeyemo <teslimni@gmail.com>
2017-04-10 11:06:53 +0100

    Added a welcome text to the slim home route on the dev branch

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php 
 use App\Controllers\TopicController;
 
+$app->get('/', function () {
+	echo 'Welcome to simple slim';
+});
+
 $app->group('/topics', function () {
 	$this->get('', TopicController::class . ':index');
 	$this->get('/{id}', TopicController::class . ':show');
