794a76379f4ad3f680b733e4309555b6dfb0110d
Teslim Adeyemo <teslimni@gmail.com>
2017-04-30 16:02:15 +0100

    Removed unnecessary line spaces

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,20 +8,16 @@ $app = new \Slim\App([
 ]);
 
 $container = $app->getContainer();
-
 $container['db'] = function () {
 	return new PDO( 'mysql:host=localhost;dbname=simpleslim', 'homestead', 'secret' );
 };
-
 $container['view'] = function ($container) {
 	$view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
 		'cache' => false
 	]);
-
 	// Instantiate and add Slim specific extension
 	$basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
 	$view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
-
 	return $view;
 };
 
